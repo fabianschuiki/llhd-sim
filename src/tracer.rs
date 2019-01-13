6593f098aa3d7a4a384865e7a7c438b29b62c031
Fabian Schuiki <fabian@schuiki.ch>
2019-01-13 13:33:13 +0100

    doc: add comments to tracer trait

--- src/tracer.rs
+++ src/tracer.rs
@@ -2,21 +2,23 @@
 
 //! A simulation tracer that can store the generated waveform to disk.
 
+use crate::state::{SignalRef, State};
 use llhd::{Const, ConstKind};
 use num::{BigInt, BigRational};
-use crate::state::{SignalRef, State};
 use std;
 use std::collections::{HashMap, HashSet};
 
+/// A simulation tracer that can operate on the simulation trace as it is being
+/// generated.
 pub trait Tracer {
     /// Called once at the beginning of the simulation.
-    fn init(&mut self, _: &State);
+    fn init(&mut self, state: &State);
 
     /// Called by the simulation engine after each time step.
-    fn step(&mut self, _: &State, _: &HashSet<SignalRef>);
+    fn step(&mut self, state: &State, changed: &HashSet<SignalRef>);
 
     /// Called once at the end of the simulation.
-    fn finish(&mut self, _: &State);
+    fn finish(&mut self, state: &State);
 }
 
 /// A no-op tracer that does nothing.
